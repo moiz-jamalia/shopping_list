2554ea281255c9f5fa873380f1862bd89e1c6e06
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2022-06-28 22:27:16 +0200

    Update shopping-list.service.ts

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,7 +4,7 @@ import {EventEmitter} from "@angular/core";
 export class ShoppingListService {
   ingChanged = new EventEmitter<Ingredient[]>();
 
-  private ingredients: Ingredient[] = [
+  private ingredients : Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
   ];
@@ -14,6 +14,18 @@ export class ShoppingListService {
   }
 
   addIngredient(ing : Ingredient){
+<<<<<<< Updated upstream
+=======
+    let shouldAdd: boolean = true;
+    let ingCopy : Ingredient = new Ingredient(ing.name, ing.amount);
+    this.ingredients.forEach(function (value) {
+      if(value.name == ing.name) {
+        ingCopy.amount = Number(value.amount) + Number(ingCopy.amount);
+        shouldAdd = false;
+      }
+    });
+    if(!shouldAdd) return;
+>>>>>>> Stashed changes
     this.ingredients.push(ing);
     this.ingChanged.emit(this.ingredients.slice());
   }
