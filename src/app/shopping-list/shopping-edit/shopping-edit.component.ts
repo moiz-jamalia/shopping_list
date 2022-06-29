f4e69f6c4ecb428f20fce865b86afb5db3472d16
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2022-06-29 09:57:19 +0200

    change Shopping Edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,13 +1,8 @@
-import {
-  Component,
-  OnInit,
-  ElementRef,
-  ViewChild,
-
-} from '@angular/core';
+import {Component, OnInit,} from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import {ShoppingListService} from "../shopping-list.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -15,19 +10,16 @@ import {ShoppingListService} from "../shopping-list.service";
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
   constructor(private slService : ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    if (ingName == "" || ingAmount == "") return;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form : NgForm) {
+    const value = form.value
+    if (value.name == "" || value.amount == "") return;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
 }
